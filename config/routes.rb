284d3206d9fdca227887d2a78541fb4423c011df
Rafael Garcia <rafbgarcia@gmail.com>
2013-10-23 15:54:25 -0300

    Página sobre

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Mulheremti::Application.routes.draw do
   root 'home#index'
 
   get 'entrevistas/lilian-de-munno' => 'interviews#lilians', as: :lilians_interview
+  get 'sobre' => 'application#about', as: :about
 
 end
